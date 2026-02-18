6ddfe72c5bc6a141fdbe4d371cd6ea994974b7df
Pawel Sikora <sikor6@gmail.com>
2026-02-19 00:12:14 +0100

    bump version to 1.0.2
    Signed-off-by: Pawel Sikora <sikor6@gmail.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="mkdocs-cdoc",
-    version="1.0.0",
+    version="1.0.2",
     description="MkDocs plugin for C/C++ autodoc (Hawkmoth-like)",
-    keywords="mkdocs cdoc c hawkmoth documentation gtk-doc python",
+    keywords="mkdocs cdoc c hawkmoth documentation python",
     url="https://github.com/pawelsikora/mkdocs-cdoc/",
     author="Pawel Sikora",
     author_email="sikor6@gmail.com",
